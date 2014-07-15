a43e53ccc7a44bb22508c440330f0c2f8f62673e
Brian Hoffman <brianjhoffman@gmail.com>
2014-07-15 15:20:01 -0400

    make the app and tasks exportable

--- bin/as_cli.js
+++ bin/as_cli.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var fs = require('fs');
 var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
-var App = require(lib + '/app.js');
+var App = require(lib + '/app.js')['App'];
 
 new App().run();
 
