8243543e24b608bdf36e6fdf8e5a887c5fb9b413
Brian Hoffman <brianjhoffman@gmail.com>
2014-08-01 11:26:44 -0400

    space out requests when asking ArchivesSpace to export everything

--- examples/export-all-eads.js
+++ examples/export-all-eads.js
@@ -4,6 +4,34 @@ var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
 var path = require('path');
 
+var Queue = function() {
+  var that = this;
+  var q = [];
+  var throttle = 0;
+
+  this.tickDown = function() {
+    if(throttle > 0) {
+      throttle -= 1;
+    }
+  }
+
+  this.push = function(callback) {
+    q.push(callback);
+  }
+
+  this.start = function() {
+    if (q.length > 0 && throttle < 10) {
+      q.pop()();
+      throttle += 1;
+    }
+    setTimeout(function() {
+      console.log("Throttle " + throttle + " - Size " + q.length);
+      that.start();
+    }, 2000);
+  }
+
+}
+
 module.exports = function(api) {
 
   var dir = argv['dir']
@@ -13,16 +41,28 @@ module.exports = function(api) {
     throw("Output directory does not exist");   
   }
 
+  var q = new Queue();
+  q.start()
+  
   api.eachResource(function(resource) {
-    console.log("Download EAD for " + resource.title);
 
-    var id = resource.uri.replace(/.*\//, '')
+    q.push(function() {
+      
+      console.log("Download EAD for " + resource.title);
 
-    var ead_uri = "/repositories/:repo_id/resource_descriptions/" + id + ".xml?include_daos=true&include_unpublished=true&numbered_cs=false";
+      var id = resource.uri.replace(/.*\//, '')
 
-    var outpath = path.join(dir, id + ".xml");
-    var ws = fs.createWriteStream(outpath);
+      var ead_uri = "/repositories/:repo_id/resource_descriptions/" + id + ".xml?include_daos=true&include_unpublished=true&numbered_cs=false";
 
-    api.download(ead_uri, ws);
+      var outpath = path.join(dir, id + ".xml");
+      var ws = fs.createWriteStream(outpath);
+
+      ws.on('finish', function() {
+        console.log("finish");
+        q.tickDown();
+      });
+
+      api.download(ead_uri, ws);
+    });
   });
 }
